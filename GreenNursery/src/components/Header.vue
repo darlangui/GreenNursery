b238fc3e2603f78df213a0bb29d6452106f46afc
darlanguimaraes <darlaneduardoguimaraes@gmail.com>
2023-08-24 11:55:30 -0300

    component header create

---
+++ GreenNursery/src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+      <span>Header</span>
+  </header>
+</template>
+
+<style scoped>
+  header{
+    font-family: "JetBrains Mono", sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+  }
+</style>
