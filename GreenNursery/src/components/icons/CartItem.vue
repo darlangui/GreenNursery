0f38610ae01114760fa063fba49dc174c5e8b3cb
darlanguimaraes <darlaneduardoguimaraes@gmail.com>
2023-08-25 13:42:55 -0300

    fix: cardView complet

---
+++ GreenNursery/src/components/icons/CartItem.vue
@@ -0,0 +1,84 @@
+s<script setup>
+
+</script>
+
+<template>
+  <div class="cart">
+    <div class="image">
+      <slot name="image"></slot>
+    </div>
+    <div class="information">
+      <p><slot name="name"></slot></p>
+      <div class="add">
+        <img src="/icons/removeicon.svg" alt="Remove Icon">
+        <span><slot name="mount"></slot> Unidade/s</span>
+        <img src="/icons/addicon.svg" alt="Add Icon">
+      </div>
+      <span>R$ <slot name="value"></slot></span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .cart {
+    max-width: 536px;
+    height: 152px;
+
+    display: flex;
+
+    margin-bottom: 32px;
+  }
+
+  .image {
+    width: 152px;
+    height: 152px;
+
+    background-color: #F8F8FA;
+    display: flex;
+    justify-content: center;
+    align-items: end;
+  }
+
+  .information{
+    max-width: 360px;
+    max-height: 125px;
+    margin-left: 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .information p{
+    color: #475569;
+    font-size: 16px;
+    margin-bottom: 16px;
+  }
+
+  .information span {
+    font-size: 24px;
+    color: #030712;
+    font-weight: 500;
+  }
+
+  .information .add {
+    width: 100%;
+    height: 32px;
+    display: flex;
+    gap: 16px;
+    align-items: center;
+
+    margin-bottom: 24px;
+  }
+
+  .information .add span{
+    font-size: 16px;
+    color: #030712;
+  }
+
+  .information .add img {
+    max-width: 32px;
+    max-height: 32px;
+
+    cursor: pointer;
+  }
+</style>
--- GreenNursery/src/router/index.js
+++ GreenNursery/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import DescriptionView from "@/views/DescriptionView.vue";
+import CartView from "@/views/CartView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/description',
       name: 'description',
       component: DescriptionView
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: CartView
     },
   ]
 })
