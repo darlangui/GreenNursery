6c2217491f87a2424b1bc2d19b36419584ab7cc7
darlanguimaraes <darlaneduardoguimaraes@gmail.com>
2023-08-24 23:16:31 -0300

    fix: init descriptionView

--- GreenNursery/src/router/index.js
+++ GreenNursery/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
+import DescriptionView from "@/views/DescriptionView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView
+    },
+    {
+      path: '/description',
+      name: 'description',
+      component: DescriptionView
     },
   ]
 })
